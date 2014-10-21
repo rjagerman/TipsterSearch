4db35ef97de81b8a44fa88b05baf0cff6808d7cd
Rolf Jagerman <rjagerman@gmail.com>
2014-10-21 16:30:11 +0200

    Simplified stopword filtering

--- src/main/scala/processing/TextProcessor.scala
+++ src/main/scala/processing/TextProcessor.scala
@@ -15,7 +15,7 @@ object TextProcessor {
       */
     def process(text:String) : List[String] = {
         Tokenizer.tokenize(text.toLowerCase).par // Tokenize lower case text
-            .filter(s => !Stopwords.set(s)) // Filter stop words
+            .filter(Stopwords.set) // Filter stop words
             .map(s => PorterStemmer.stem(s)).toList // Stem words
     }
     

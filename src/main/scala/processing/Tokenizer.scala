848e23e07e9bbd4ad3096d551efe135e86db9d85
Rolf Jagerman <rjagerman@gmail.com>
2014-10-21 23:38:21 +0200

    Added question mark and exclamation mark as split symbols for tokenizer

--- src/main/scala/processing/Tokenizer.scala
+++ src/main/scala/processing/Tokenizer.scala
@@ -1,16 +1,18 @@
 package ch.ethz.inf.da.tipstersearch.processing
 
-/** Tokenizes strings and removes punctuation
+/**
+  * Tokenizes strings and removes punctuation
   */
 object Tokenizer {
 
-    /** Tokenize given string, this also converts the tokens to lowercase
+    /**
+      * Tokenize given string, this also converts the tokens to lowercase
       * 
       * @param text the text to tokenize
       * @return the tokens as a list of strings
       */
     def tokenize(text:String) : List[String] = {
-        text.split("[ \\_;:\\(\\),\\.\\-]+").toList
+        text.split("[ \\_;:\\(\\),\\.\\-\\?\\!]+").toList
     }
 
 }

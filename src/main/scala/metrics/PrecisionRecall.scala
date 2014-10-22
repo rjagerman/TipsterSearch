658a027af17b57f4a1247f8eab4f590f3aabaf35
Rolf Jagerman <rjagerman@gmail.com>
2014-10-22 18:04:07 +0200

    Fixed the formula for the average precision

--- src/main/scala/metrics/PrecisionRecall.scala
+++ src/main/scala/metrics/PrecisionRecall.scala
@@ -12,8 +12,9 @@ import ch.ethz.inf.da.tipstersearch.Query
   */
 class PrecisionRecall(query:Query) {
 
-    val retrievedDocuments:Set[String] = query.results.ordered.map(r => r.id).toSet
-    val relevantDocuments:Set[String] = query.truth.filter(_._2 == 1).map{case (id,c) => id}.toSet
+    val rankings:List[String] = query.results.ordered.map(r => r.id)
+    val retrieved:Set[String] = rankings.toSet
+    val relevant:Set[String] = query.truth.filter(_._2 == 1).map{case (id,c) => id}.toSet
 
     /**
       * Computes the precision over the results
@@ -21,7 +22,7 @@ class PrecisionRecall(query:Query) {
       * @return the precision
       */
     def precision : Double = {
-        return (retrievedDocuments & relevantDocuments).size.toDouble / retrievedDocuments.size.toDouble
+        return (retrieved & relevant).size.toDouble / retrieved.size.toDouble
     }
 
     /**
@@ -31,7 +32,7 @@ class PrecisionRecall(query:Query) {
       * @return the precision
       */
     def precision(k:Int) : Double = {
-        return (retrievedDocuments.take(k) & relevantDocuments).size.toDouble / retrievedDocuments.take(k).size.toDouble
+        return (rankings.take(k).toSet & relevant).size.toDouble / k
     }
 
     /**
@@ -40,7 +41,17 @@ class PrecisionRecall(query:Query) {
       * @return the recall
       */
     def recall : Double = {
-        return (retrievedDocuments & relevantDocuments).size.toDouble / relevantDocuments.size.toDouble
+        return (retrieved & relevant).size.toDouble / relevant.size.toDouble
+    }
+
+    /**
+      * Indicator function to determine if document at index k is relevant
+      * 
+      * @param k the index to check (1-based)
+      * @return 1 if the document at k is relevant, 0 if it is not
+      */
+    def isRelevant(k:Int) : Int = {
+        if (relevant.contains(rankings(k-1))) return 1 else return 0
     }
 
     /**
@@ -50,17 +61,14 @@ class PrecisionRecall(query:Query) {
       */
     def averagePrecision : Double = {
         var average:Double = 0.0
-        var prevPrecision = 0.0
-        var count = 0
-        1 to retrievedDocuments.size foreach {
-            k => if(precision(k) > prevPrecision) {
-                     average += precision(k)
-                     count += 1
-                 }
-                 prevPrecision = precision(k)
+        (1 to retrieved.size) foreach {
+            k => average += precision(k) * isRelevant(k)
         }
-        if(count == 0) { return 0.0 }
-        else { return average / count }
+        val nrOfRelevantDocuments = (retrieved & relevant).size
+
+        // Prevent division by 0 if none of the documents were relevant...
+        if(nrOfRelevantDocuments == 0) 0
+        else average / nrOfRelevantDocuments.toDouble
     }
 
 }
